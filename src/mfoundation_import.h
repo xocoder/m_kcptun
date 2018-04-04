87742da1312d62fc6c3586088cdf675b15ea783b
lalawue <suchaaa@gmail.com>
2018-04-04 22:16:39 +0800

    only import required module

---
+++ src/mfoundation_import.h
@@ -0,0 +1,42 @@
+/* 
+ * Copyright (c) 2018 lalawue
+ * 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the MIT license. See LICENSE for details.
+ */
+
+/* m_foundation import example
+ */
+
+/* for all */
+#define M_FOUNDATION_IMPORT_CRYPTO 1
+#define M_FOUNDATION_IMPORT_MODEL  1
+#define M_FOUNDATION_IMPORT_PLAT   1
+
+/* crypto module */
+#ifdef M_FOUNDATION_IMPORT_CRYPTO
+#define M_FOUNDATION_IMPORT_CRYPTO_PRNG    1
+#define M_FOUNDATION_IMPORT_CRYPTO_RC4     1
+#define M_FOUNDATION_IMPORT_CRYPTO_XOR64   1
+#endif  /* M_FOUNDATION_IMPORT_CRYPTO */
+
+
+/* model module */
+#ifdef M_FOUNDATION_IMPORT_MODEL
+
+#define M_FOUNDATION_IMPORT_MODEL_MEM 1
+
+#define M_FOUNDATION_IMPORT_MODEL_SKIPLIST (M_FOUNDATION_IMPORT_MODEL_MEM && \
+                                            M_FOUNDATION_IMPORT_CRYPTO_PRNG)
+
+#endif  /* M_FOUNDATION_IMPORT_MODEL */
+
+
+
+/* plat module */
+#ifdef M_FOUNDATION_IMPORT_PLAT
+
+/* time for win/nix */
+#define M_FOUNDATION_IMPORT_PLAT_TIME    1
+
+#endif  /* M_FOUNDATION_IMPORT_PLAT */
